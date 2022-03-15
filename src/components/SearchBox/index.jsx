6c69af6d0de2bf51831e4e971e23d8c9875a893e
Sairu <39128592+Saiiru@users.noreply.github.com>
2022-03-15 14:41:05 -0300

    Update index.jsx

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchBox({onSearchChange, searchField}) {
   return (<div className="pa3">
-    <input  className="pa3 ba b--green bg-lightest-blue "  type="search" placeholder="puta" onChange={onSearchChange}/>
+    <input  className="pa3 ba b--green bg-lightest-blue "  type="search" placeholder="Search Planets" onChange={onSearchChange}/>
   </div>
   )
 }
